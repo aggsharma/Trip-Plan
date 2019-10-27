f6b58566843f98773784d2a3ee8326005c5cc16b
aggsharma <asharma97@gmail.com>
2019-10-27 12:50:17 -0500

    springboot unzip

---
+++ src/test/java/org/launchcode/tripplan/TripPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.tripplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
